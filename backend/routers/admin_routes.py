aea01176ab65336ca18b617982f836b8d74b6b66
MENEZES LIMA <raphael.menezeslima@dxc.com>
2025-10-30 03:22:56 -0300

    19.99

--- backend/routers/admin_routes.py
+++ backend/routers/admin_routes.py
@@ -117,7 +117,7 @@ async def create_subscription_checkout(
                 "description": "Acesso completo à plataforma Horalis por 30 dias.",
                 "quantity": 1,
                 "currency_id": "BRL",
-                "unit_price": 00.99 # <<< SEU PREÇO
+                "unit_price": 19.99 # <<< SEU PREÇO
             }
         ],
         "payer": {
